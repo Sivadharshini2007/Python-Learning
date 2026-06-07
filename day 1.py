9fc1d5bdb90c7bf5ba99af52accd309b3a7f044f
Sivadharshini2007 <sivadharshinir57@gmail.com>
2026-06-07 11:38:55 +0530

    Update day 1.py

--- day 1.py
+++ day 1.py
@@ -1,2 +1,31 @@
 print("hello world")
-print("my name is sivadharshini")
+print("my name is Sivadharshini")
+
+#variables
+name = "kiran"
+age = 18
+
+print(name)
+print(age)
+
+output:
+Kiran
+18
+
+#input
+name = input("Enter your name: ")
+print(name)
+
+output:
+Enter your name:ammu
+ammu
+
+#type casting
+a=10
+b="siva"
+print(type(a))
+print(type(b))
+
+output:
+int
+str
